fe6a38bab8d46426aded6144b72fdd1e4e6f12ce
xavier geerinck <thebillkidy@hotmail.com>
2011-04-29 22:52:06 +0200

    - Code vereenvoudigd. - Meer loggings toegepast. - Server en Clientsocket kunnen sluiten nu. - Kleinere aanpassingen.

--- SocketC#/Server/Network/Listener.cs
+++ SocketC#/Server/Network/Listener.cs
@@ -13,6 +13,9 @@ namespace Server.Network
 
         public static void startlistening()
         {
+            //aanaf zodat we de server god kunnen stoppen.
+            bool aanaf = true;
+
             //Definieer Sockets voor client en server + Listening IP en poort (in dit geval ip 0.0.0.0 en poort 13000)
             TcpListener serverSocket = new TcpListener(System.Net.IPAddress.Any, 13000);
             TcpClient clientSocket = default(TcpClient);
@@ -26,33 +29,42 @@ namespace Server.Network
             counter = 0;
 
             //main loop
-            while ((true))
+            while (aanaf == true)
             {
-                counter += 1;
-                clientSocket = serverSocket.AcceptTcpClient();
+                try
+                {
+                    counter += 1;
+                    clientSocket = serverSocket.AcceptTcpClient();
 
-                byte[] bytesFrom = new byte[10025];
-                string dataFromClient = null;
+                    byte[] bytesFrom = new byte[10025];
+                    string dataFromClient = null;
 
-                NetworkStream networkStream = clientSocket.GetStream();
-                networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
-                dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
-                dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
-                
-                clientsList.Add(dataFromClient, clientSocket);
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
+                    dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                    dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
 
-                Server.User.BroadCast.broadcast(dataFromClient + " Joined ", dataFromClient, false);
-                Console.WriteLine();
-                Server.Logger.addlog(dataFromClient + " Joined chat room ", Server.Logger.LogType.User);
-                handleClinet client = new handleClinet();
-                client.startClient(clientSocket, dataFromClient, clientsList);
+                    clientsList.Add(dataFromClient, clientSocket);
+
+                    Server.User.BroadCast.broadcast(dataFromClient + " Joined ", dataFromClient, false);
+                    Console.WriteLine();
+                    Server.Logger.addlog(dataFromClient + " Joined chat room ", Server.Logger.LogType.User);
+                    handleClinet client = new handleClinet();
+                    client.startClient(clientSocket, dataFromClient, clientsList);
+                }
+                catch
+                {
+                    Server.Logger.addlog("An error occured in Linstener.cs!", Logger.LogType.Error);
+                    aanaf = false;
+
+                    //Zet de ClientSocket en de ServerSocket uit!
+                    clientSocket.Close();
+                    serverSocket.Stop();
+
+                    //Zend log door
+                    Server.Logger.addlog("exit", Server.Logger.LogType.Error);
+                }
             }
-
-            //stop de ClientSocket en de ServerSocket (Probleem want ik zit in een while true loop :P
-            clientSocket.Close();
-            serverSocket.Stop();
-            Server.Logger.addlog("exit", Server.Logger.LogType.Error);
-            Console.ReadLine();
         }
     }
 }
