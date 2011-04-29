fc563b126938745cfbbe85924ea58cd6f7791a52
xavier geerinck <thebillkidy@hotmail.com>
2011-04-29 22:35:36 +0200

    - Kleuren toegevoegd aan de debug lijnen.\n- Alle functies in aparte mappen en bestanden gestoken.\n- Code mooier gemaakt.\n- Toevoeging van Logger.

---
+++ SocketC#/Server/Logger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Server
+{
+    class Logger
+    {
+        public enum LogType
+        {
+            Socket,
+            Error,
+            User
+        }
+
+        static public void addlog(string log, LogType logtype)
+        {
+            switch (logtype)
+            {
+                case LogType.Socket:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("[Socket]: " + log);
+                    break;
+                case LogType.User:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("[User]: " + log);
+                    break;
+                case LogType.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("[Error]: " + log);
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    break;
+            }
+            
+        }
+    }
+}
---
+++ SocketC#/Server/Network/Listener.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections;
+
+namespace Server.Network
+{
+    class Listener
+    {
+        //definieer user table
+        public static Hashtable clientsList = new Hashtable();
+
+        public static void startlistening()
+        {
+            //Definieer Sockets voor client en server + Listening IP en poort (in dit geval ip 0.0.0.0 en poort 13000)
+            TcpListener serverSocket = new TcpListener(System.Net.IPAddress.Any, 13000);
+            TcpClient clientSocket = default(TcpClient);
+            int counter = 0;
+
+            //Socket start
+            serverSocket.Start();
+            Server.Logger.addlog("Chat Server Started ....", Server.Logger.LogType.Socket);
+
+            //reset counter
+            counter = 0;
+
+            //main loop
+            while ((true))
+            {
+                counter += 1;
+                clientSocket = serverSocket.AcceptTcpClient();
+
+                byte[] bytesFrom = new byte[10025];
+                string dataFromClient = null;
+
+                NetworkStream networkStream = clientSocket.GetStream();
+                networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
+                dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+                
+                clientsList.Add(dataFromClient, clientSocket);
+
+                Server.User.BroadCast.broadcast(dataFromClient + " Joined ", dataFromClient, false);
+                Console.WriteLine();
+                Server.Logger.addlog(dataFromClient + " Joined chat room ", Server.Logger.LogType.User);
+                handleClinet client = new handleClinet();
+                client.startClient(clientSocket, dataFromClient, clientsList);
+            }
+
+            //stop de ClientSocket en de ServerSocket (Probleem want ik zit in een while true loop :P
+            clientSocket.Close();
+            serverSocket.Stop();
+            Server.Logger.addlog("exit", Server.Logger.LogType.Error);
+            Console.ReadLine();
+        }
+    }
+}
